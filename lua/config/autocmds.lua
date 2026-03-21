32a3e55e0c9ea1bd5d20a28c881457bc71197fb2
NotJiXun <25026683@myrp.edu.sg>
2026-03-21 13:38:55 +0800

    configured autosave(every 15s) and prettier manual trigger(<leader>fp)

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,14 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-  pattern = { "*" },
-  command = "silent! wall",
-  nested = true,
-})
+-- Autosave every 15 seconds
+vim.fn.timer_start(15000, function()
+  vim.cmd("silent! wall")
+end, { ["repeat"] = -1 }) -- -1 means repeat forever
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,7 @@
 vim.keymap.set("n", "<leader>ut", "<cmd>lua _G.toggle_transparency()<cr>", { desc = "Toggle Transparency" })
 vim.keymap.set("n", "<leader>uc", "<cmd>lua _G.cycle_theme()<cr>", { desc = "Cycle Theme" })
+
+-- Format current file with prettier (via conform.nvim)
+vim.keymap.set({ "n", "v" }, "<leader>fp", function()
+  require("conform").format({ formatters = { "prettier" }, lsp_fallback = false })
+end, { desc = "Format with Prettier" })
